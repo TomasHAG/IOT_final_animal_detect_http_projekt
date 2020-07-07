399bdafb2abcaa488a0929a28a1790cccaef4993
TomasHAG <42994176+TomasHAG@users.noreply.github.com>
2020-07-07 16:29:28 +0200

    Update keys.py

--- LoRa/lib/keys.py
+++ LoRa/lib/keys.py
@@ -1,7 +1,7 @@
 import ubinascii
 # Here are the keys that are used to initilize LORAWAN
 def eui():
-    return ubinascii.unhexlify('70B3D57ED0030CC1')
+    return ubinascii.unhexlify('your EUI key here')
 
 def key():
-    return ubinascii.unhexlify('214C55A7F7D6107B6BDA01338307DB25')
+    return ubinascii.unhexlify('your API key here')
